417c91bfa640dfaa0ee16f7eca0b1eea086dd0c9
mertovich <88425310+mertovich@users.noreply.github.com>
2022-05-13 17:13:31 +0300

    add(ToDo)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FormToDo from './components/FormToDo'
 import NavBar from './components/NavBar'
+import ToDoList from './components/ToDoList'
 
 type Props = {}
 
 const App: React.FC<Props> = (props: Props) => {
+  const [todoList, setTodoList] = useState<string[]>([]);
+  const [toDo, setToDo] = useState<string>('');
+
+  const addToDo = (): void => {
+    setTodoList([...todoList, toDo]);
+  }
+
+  const addTask = (task: string): void => {
+    setToDo(task);
+  }
+
   return (
     <div>
       <NavBar />
+      <FormToDo addToDo={addToDo} addTask={addTask} />
+      <ToDoList todoList={todoList} />
     </div>
   )
 }
